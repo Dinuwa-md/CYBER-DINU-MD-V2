f8223a3f650adabefd4c2a8094e2d65b60853967
Dinuwa-md <dinuwamdnew2007@gmail.com>
2025-02-26 16:38:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    sessionId: "your-session-id",  // Add unique session ID for persistence
+    sessionId: "PRABATH-MD~E7EXmTzI#zsxDPghL7a-HxSAArGZrb87Z22BHA6Nc6Uxads6fcEI",  // Add unique session ID for persistence
     groupLink: "https://chat.whatsapp.com/your-group-link",  // Support Group Link
     channelLink: "https://wa.me/your-channel-link",  // WhatsApp Channel Link
 };
